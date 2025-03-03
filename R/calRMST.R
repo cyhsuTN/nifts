87f42764a3418bb0ffed09c18ac68aa7033c89b3
Chih-Yuan Hsu <53067662+cyhsuTN@users.noreply.github.com>
2025-03-03 09:54:02 -0600

    Create calRMST.R

---
+++ R/calRMST.R
@@ -0,0 +1,33 @@
+#' Calculate RMST
+#' @description A function to calculate RMST
+#'
+#' @import stats
+#' @importFrom flexsurv pgengamma.orig rgengamma.orig
+#'
+#' @param m Median survival time.
+#' @param shape The shape parameter in generalized gamma distributions
+#' (flexsurv::pgengamma.orig) for event time.
+#' @param k The k parameter in generalized gamma distributions
+#' (flexsurv::pgengamma.orig) for event time.
+#' @param tau A value to specify the truncation time point for the RMST calculation.
+
+#' @examples # calRMST(m=1, shape=1, k=1, tau=2.5)
+
+#' @export
+calRMST <- function(m=1, shape=1, k=1, tau=NULL) {
+
+  scale1 <- calScale(m, shape=shape, k=k)
+
+  integratef <- function(t, scale, shape, k) {
+
+    Surv.gengamma.orig0 <- function(c) {
+      pgengamma.orig(c, shape=shape, scale=scale, k=k, lower.tail=FALSE)
+    }
+    integrate(Surv.gengamma.orig0, 0, t)$value
+
+  }
+  u1 <- integratef(tau, scale1, shape, k)
+
+  return(u1)
+
+}
