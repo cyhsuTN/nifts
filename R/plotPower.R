0dcafeaaf85a4323a02b295204e922fb9aec61f0
Chih-Yuan Hsu <53067662+cyhsuTN@users.noreply.github.com>
2025-04-29 13:04:40 -0500

    Update plotPower.R

--- R/plotPower.R
+++ R/plotPower.R
@@ -7,9 +7,9 @@
 #' @param x.by Grid width of x-axis.
 #' @param y.by Grid width of y-axis.
 
-#' @examples # ab <- paramWeibull(m1=1, t1=2.5, surv.prob=0.1)
+#' @examples # ab <- paramWeibull(m=1, t1=2.5, surv.prob=0.1)
 #' @examples # pow <- calculate_size(nL=20, nU=200, r=1, m1=1, m2=1.5,
-#' @examples #                  shape=ab[1], m0=0.5, f2=0.5, p.s=0.3, tau=2.5)
+#' @examples #                  shape=ab[1], m0=0.5, f2=0.5, p.s=0.3, tau=3)
 #' @examples # plotPower(pow, x.by=20, y.by=0.1)
 
 #' @export
